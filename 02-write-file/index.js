1e46e028edfbf22edda80b209592f3aba0e5172a
Denna <lilithprimary@gmail.com>
2022-05-15 19:00:55 +0200

    feat: 2 tasks done (Sun, May 15, 2022 07:00:55 PM)

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const process = require("process");
+const readline = require('readline');
+const fs = require("fs");
+const path = require('path');
+
+const rl = readline.createInterface({ input: process.stdin, output:process.stdout });
+let writeStream;
+
+rl.question("Input file name\n", (ans) => {
+  writeStream = fs.createWriteStream(path.join(__dirname, `${ans}.txt`));
+  console.log(`Your file is ${ans}.txt`);
+  askQuestion(ans);
+})
+
+function askQuestion(filename) {
+  rl.question(`Input text for writing in ${filename}.txt\n`, (ans) => {
+    writeStream.write(ans + "\n");
+    askQuestion(filename);
+  })
+}
+
+//Эта дурацкая команда не работает в консоли баша в вскоде. Проверьте, плиз, в cmd или power shell (хотя можт это у меня не работает, а у вас всё ок будет)
+rl.on("SIGINT", () => {
+  console.log("\nУдачи в нашем нелёгком деле =)\n");
+  process.exit();
+});

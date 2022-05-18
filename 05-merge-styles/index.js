1a42de8616124fcd1567b0edf58effbffbeecc2e
Denna <lilithprimary@gmail.com>
2022-05-18 13:36:24 +0200

    feat: 5th is done (Wed, May 18, 2022 01:36:24 PM)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const fsProm = require('fs/promises');
+const path = require('path');
+
+const curDir = path.resolve(__dirname, 'styles');
+const targDir = path.resolve(__dirname, 'project-dist');
+const writeStream = fs.createWriteStream(path.resolve(targDir, 'bundle.css'));
+
+async function bundle() {
+  const files = await fsProm.readdir(curDir, {withFileTypes: true});
+  files.forEach(el => {
+    if(el.isFile()) {
+      const filePath = path.resolve(curDir, el.name);
+      if (path.extname(filePath) === '.css') {
+        const readStream = fs.createReadStream(filePath);
+        readStream.pipe(writeStream);
+      }
+    }
+  });
+}
+bundle();

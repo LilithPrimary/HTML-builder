d52eb37cb81268d6ccdfde1f54e76e601cb2593f
Denna <lilithprimary@gmail.com>
2022-05-18 11:51:16 +0200

    feat: 4th is done (Wed, May 18, 2022 11:51:15 AM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,62 +1,68 @@
+//прошу прощение за такое замороченное решение. Решила сделать с проверкой существования файла.
+// Если файл существует, то программа даёт выбор - либо пересоздать файл, либо дозаписывать в имеющийся.
+// В любом случае, то, что нужно, программа выполняет, а это просто доп. извращения для тренировки =)
+
 const path = require('path');
 const readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-const {stdin, stdout} = process;
-const fs = require("fs");
+const {stdout} = process;
+const fs = require('fs');
 
-fs.access(path.join(__dirname, `text.txt`), (err) => {
+fs.access(path.join(__dirname, 'text.txt'), (err) => {
   if (err) {
-    writeStream = fs.createWriteStream(path.join(__dirname, `text.txt`));
+    fs.createWriteStream(path.join(__dirname, 'text.txt'));
     addText();
   } else {
-    rl.question("text.txt is already exist. Do you want to remove this file? (y/n)\nIn case 'no' your text will be added to existing file ", (ans) => {
+    rl.question('text.txt is already exist. Do you want to remove this file? (y/n)\nIn case \'no\' your text will be added to existing file ', (ans) => {
       switch (ans) {
-        case "y":
-          fs.unlink(path.join(__dirname, `text.txt`), (err) =>  {
-            if (err) {
-              stdout.write("Something went wrong");
-              process.exit();
-            } else {
-              stdout.write("file removed \n");
-              writeStream = fs.createWriteStream(path.join(__dirname, `text.txt`));
-              addText();
-            }
-          });
-          break;
-        case "n":
-          addText();
-          break;
-        default:
-          stdout.write("You should input y or n");
-          process.exit();
+      case 'y':
+        fs.unlink(path.join(__dirname, 'text.txt'), (err) =>  {
+          if (err) {
+            stdout.write('Something went wrong');
+            process.exit();
+          } else {
+            stdout.write('file removed \n');
+            fs.createWriteStream(path.join(__dirname, 'text.txt'));
+            addText();
+          }
+        });
+        break;
+      case 'n':
+        addText();
+        break;
+      default:
+        stdout.write('You should input y or n');
+        process.exit();
       }
-    })
+    });
   }
 
-})
+});
 
 function addText () {
-  stdout.write("Input your text here: \n");
-  rl.on("line", (text) => {
+  stdout.write('Input your text here: \n');
+  rl.on('line', (text) => {
     checkText(text);
-    fs.appendFile(path.join(__dirname, `text.txt`), text + "\n", (err) => {
+    fs.appendFile(path.join(__dirname, 'text.txt'), text + '\n', (err) => {
       if (err) return;
-    })
-  })
+    });
+  });
 }
 
-rl.on("SIGINT", () => {
-  console.log("\nУдачи в нашем нелёгком деле =)\n");
-  process.exit();
-})
-
+rl.on('SIGINT', () => {
+  programmExit();
+});
 
 function checkText(text) {
-  if (text === "exit") {
-    console.log("\nУдачи в нашем нелёгком деле =)\n");
-    process.exit();
+  if (text.trim() === 'exit') {
+    programmExit();
   }
+}
+
+function programmExit() {
+  console.log('\nУдачи в нашем нелёгком деле =)\n');
+  process.exit();
 }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+const oldPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
 
+fs.access(newPath, (err) => {
+  if (err) {
+    copying(oldPath, newPath);
+  }
+  else fs.rm(newPath, { recursive: true }, (err) => {
+    if (err) throw err;
+    copying(oldPath, newPath);
+  });
+});
+
+function createDir(dir) {
+  fs.mkdir(dir, (err) => {
+    if (err) throw err;
+  });
+}
+
+function fileCopy (curDir, targDir, file) {
+  fs.copyFile( path.join(curDir, file.name), path.join(targDir, file.name), err => {
+    if (err) throw err;
+  });
+}
+
+function copying(curDir, targDir) {
+  createDir(targDir);
+  fs.readdir(curDir, {withFileTypes: true}, (err, items) => {
+    items.forEach(el => {
+      if (el.isFile()) {
+        fileCopy(curDir, targDir, el);
+      } else {
+        copying(path.join(curDir, el.name), path.join(targDir, el.name));
+      }
+    });
+  });
+}
